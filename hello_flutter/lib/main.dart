f371be7affbb734c89c56a9857faad33a4af85df
JolynLucretia <olynnn3107@gmail.com>
2023-10-16 18:07:20 +0700

    Latihan PAB
    Jolyn

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -12,8 +12,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: "Hallo Widget",
         theme: ThemeData(primarySwatch: Colors.red),
-        home: FirstScreen()
+        home: const FirstScreen()
         );
+  }
+}
+
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Demo'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                'Ini adalah contoh penggunaan Container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Tombol Elevated'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              Text('Rating: 4.5'),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -24,7 +85,7 @@ class FirstScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-        title: const Text("Samuel Effendi"),
+        title: const Text("Jolyn Lucretia"),
         actions: <Widget>[
           IconButton(
             onPressed:(){}, 
@@ -43,7 +104,7 @@ class FirstScreen extends StatelessWidget{
         ),
         ),
           body: const Center(
-            child: Text("Hallo Samuel"),
+            child: Text("Hallo Jolyn"),
           ),
         );
   }
